00b87af3feef0fc3641b928f73c31d0732d76ec1
LordSinSentido <57022857+LordSinSentido@users.noreply.github.com>
2022-11-06 20:41:33 -0600

    Cambio de nombre de la carpeta

---
+++ chat-rmi/Servidor/Principal.java
@@ -0,0 +1,24 @@
+package Servidor;
+
+import java.rmi.Remote;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+/**
+ * Principal
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        int puerto = 1111;
+
+        try {
+            System.out.println("Inicializando el servidor...");
+            Registry rmi = LocateRegistry.createRegistry(puerto);
+            rmi.rebind("Chat", (Remote) new ImplementacionChat());
+            System.out.println("¡Listo! Servidor escuchando en el puerto: " + String.valueOf(puerto));
+        } catch (Exception e) {
+            System.err.println("Ocurrió un error: " + e);
+        }
+    }
+}
